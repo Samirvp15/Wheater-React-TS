8bf9b3a380e2c8f62f5923f193d813307e481e64
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-11-07 14:43:18 -0500

    add getAxios data from WeatherAPI

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -8,12 +8,19 @@ export default function useWeather() {
         
         try {
 
-            const appID = `08b2460f271fe415587ee206255d8e06`
+            const appID = import.meta.env.VITE_API_KEY
 
             const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appID}`
             
-            const data = await axios.get(geoURL)
-            console.log(data)
+            const {data} = await axios.get(geoURL)
+            
+            const lat = data[0].lat
+            const lon = data[0].lon
+
+            const weatherURL= `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appID}`
+
+            const {data: weatherResult} = await axios.get(weatherURL)
+            console.log(weatherResult)
 
         } catch (error) {
             console.log(error)
